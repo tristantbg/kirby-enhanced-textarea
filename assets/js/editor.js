526dc01de55dbd969c02e7515867d09a0afc26d2
Thomas Günther <mail@medienbaecker.com>
2017-08-10 22:39:26 +0200

    Log attributes to console

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -156,7 +156,7 @@
         'textComplete:select': function (e, value, strategy) {
           for (var i = 0; i < kirbyTags.length; i++) {
             if (kirbyTags[i] == value) {
-              // console.log(attributes[i]);
+              console.log(attributes[i]);
             }
           }
         }
